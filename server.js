1269272b8c181c3c1936bfa0c43ba06e76cc2fdb
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-01-29 23:50:29 +0700

    Add hospital's API

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+// Route files
+const hospitals = require("./routes/hospitals");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+// Mount routers
+app.use("/api/v1/hospitals", hospitals);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
