f48b3b1839fae68648e6d8d1e3f59355b085f9c5
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-06 12:59:49 +0700

    add Authentication

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -8,11 +8,13 @@ const {
 } = require("../controllers/hospitals");
 const router = express.Router();
 
-router.route("/").get(getHospitals).post(createHospital);
+const { protect, authorize } = require("../middleware/auth");
+
+router.route("/").get(getHospitals).post(protect, authorize("admin"), createHospital);
 router
   .route("/:id")
   .get(getHospital)
-  .put(updateHospital)
-  .delete(deleteHospital);
+  .put(protect, authorize("admin"), updateHospital)
+  .delete(protect, authorize("admin"), deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -10,23 +11,23 @@ connectDB();
 
 // Route files
 const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
 
 const app = express();
 
 //Body parser
 app.use(express.json());
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+
+//Cookie parser
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(
-    "Server running in ",
-    process.env.NODE_ENV,
-    " mode on port ",
-    PORT
-  )
+  console.log("Server running in ", process.env.NODE_ENV, " mode on port ", PORT)
 );
 
 //Handle unhandled promise rejections
